48323a36d643a5b221c8ba337bba859f8343d9eb
Selvam Palanisamy <pselvam1993@gmail.com>
2026-04-06 22:14:46 +0530

    base url changed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => ({
   plugins: [react()],
   // Use root in dev, but project subpath when building for GitHub Pages.
-  base: command === "build" ? "/Image_slider_view/" : "/",
+  base:  "/",
 }));
